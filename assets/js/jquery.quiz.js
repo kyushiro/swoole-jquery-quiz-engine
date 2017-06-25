7ee2e48ad18f6d89682d44ccdf802f414908f59f
Christophe <christophe.kun@gmail.com>
2017-06-25 14:27:51 +0000

    skip next button

--- assets/js/jquery.quiz.js
+++ assets/js/jquery.quiz.js
@@ -147,7 +147,7 @@
           }
         }
 
-        base.methods.nextQuestion();
+        return (++currentQuestion === numQuestions)? base.methods.finish() : base.methods.nextQuestion();
 
         $('#quiz-response').html(response);
         $('#quiz-controls').fadeIn();
