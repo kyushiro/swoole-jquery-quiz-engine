b951d58c2f2eb2cc6d675a2de6cb78dd0cc81b80
bilkiss <bilkiss.dulloo@gmail.com>
2017-06-26 11:36:26 +0400

    Update quiz json format

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,75 +1,18 @@
 var quiz_url = $('body').data('quiz-url');
+var questions = [];
+
 
 $.getJSON(quiz_url,function(json){
   console.log('we have the questions json');
   console.log(json);
   console.log('now we convert it to the format used by our friend $.quiz below');
-});
+  questions = json;
 
-$('#quiz').quiz({
-  //resultsScreen: '#results-screen',
-  //counter: false,
-  //homeButton: '#custom-home',
-  counterFormat: 'Question %current of %total',
-  questions: [
-    {
-      'q': 'Is jQuery <strong>required</strong> for this plugin?',
-      'options': [
-        'Yes',
-        'No'
-      ],
-      'correctIndex': 0,
-    },
-    {
-      'q': 'How do you use it?',
-      'options': [
-        'Include jQuery, that\'s it!',
-        'Include jQuery and the plugin javascript.',
-        'Include jQuery, the plugin javascript, the optional plugin css, required markup, and the javascript configuration.'
-      ],
-      'correctIndex': 2,
-
-    },
-    {
-      'q': 'The plugin can be configured to require a perfect score.',
-      'options': [
-        'True',
-        'False'
-      ],
-      'correctIndex': 0,
-
-    },
-    {
-      'q': 'How do you specify the questions and answers?',
-      'options': [
-        'MySQL database',
-        'In the HTML',
-        'In the javascript configuration'
-      ],
-      'correctIndex': 2,
-
-    },
-
-    {
-        "q" : "Given this:<br>"+
-        "<img src='http://thomasburleson.github.io/angularjs-Quizzler/assets/data/images/q1_1.jpg' /><p>Which message will be returned by injecting this service and executing 'myService.getMessage()'?</p>",
-        "options"  : [
-            "Message one!",
-            "Message two!",
-            "Message three!"
-        ],
-      "correctIndex"   : 2,
-
-    },
-    {
-      "q" : "Given <div id='outer'><div class='inner'></div></div>, which of these two is the most performant way to select the inner div ?",
-      "options"  : [
-          "getElementById('outer').children[0]",
-          "getElementsByClassName('inner')[0]"
-      ],
-      "correctIndex"   : 0,
- 
-    }
-
-  ]
+  $('#quiz').quiz({
+    //resultsScreen: '#results-screen',
+    //counter: false,
+    //homeButton: '#custom-home',
+    counterFormat: 'Question %current of %total',
+    questions: questions
+  });
 });
