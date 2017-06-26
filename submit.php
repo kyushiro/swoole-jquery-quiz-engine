4e54631ab0de87cfcd60e4412e610bf60946ef8f
bilkiss <bilkiss.dulloo@gmail.com>
2017-06-26 12:24:27 +0400

    dev results

--- submit.php
+++ submit.php
@@ -1,2 +1,20 @@
 <?php
-    var_dump($_POST["start"]);
+
+    $filePath = "./assets/data/";
+
+    $data = file_get_contents($filePath . "results.json");
+    $data = json_decode($data, true);
+
+    $answers = file_get_contents($filePath . "quiz.json");
+    $answers = json_decode($answers, true);
+
+    $correctAnswers = [];
+
+    foreach ($answers as $answer){
+        $correctAnswers[] = $answer["correctIndex"];
+
+}
+    var_dump($correctAnswers);
+
+    $email = $_POST["email"];
+    $data[$email] = [];
