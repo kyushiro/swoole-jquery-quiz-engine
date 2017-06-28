6e724c4aac8dff0d4305359d3131eebe0db2d279
Christophe <christophe.kun@gmail.com>
2017-06-28 12:17:07 +0000

    server file response configured

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,20 +4,43 @@ var questions = [];
 var ws = new WebSocket("ws:/ws-devcon.cf:1995");
 
 ws.onopen = function(){
-  alert("connected!")
+  console.log("connected!")
+  send('get-quiz-data');
 }
 
-$.getJSON(quiz_url,function(json){
-  console.log('we have the questions json');
-  console.log(json);
-  console.log('now we convert it to the format used by our friend $.quiz below');
-  questions = json;
+ws.onerror = function(){
+  alert("Could not contact the quiz server, please try again");
+};
 
-  $('#quiz').quiz({
-    //resultsScreen: '#results-screen',
-    //counter: false,
-    //homeButton: '#custom-home',
-    counterFormat: 'Question %current of %total',
-    questions: questions
-  });
-});
+ws.onmessage = function(e){
+    var obj = JSON.parse(e.data);
+    console.log(obj);
+
+    if (obj.type="quiz-json"){
+      questions = obj.payload;
+      $('#quiz').quiz({
+        //resultsScreen: '#results-screen',
+        //counter: false,
+        //homeButton: '#custom-home',
+        counterFormat: 'Question %current of %total',
+        questions: questions
+      });
+    }
+};
+
+
+function send(type, content) {
+  $msg = {'type': type};
+  if (content) $msg['payload'] = JSON.stringify(content);
+  ws.send(msg);
+}
+
+
+// $.getJSON(quiz_url,function(json){
+//   console.log('we have the questions json');
+//   console.log(json);
+//   console.log('now we convert it to the format used by our friend $.quiz below');
+//   questions = json;
+
+
+// });
--- server.php
+++ server.php
@@ -26,26 +26,28 @@ Class Server
     Public function onOpen(swoole_websocket_server $server, $request){
 
         echo "server: handshake success with fd{$request->fd}\n\n";
-        $data = file_get_contents($this->file, FILE_USE_INCLUDE_PATH);
+        $data = file_get_contents('assets/data/quiz-b1.json', FILE_USE_INCLUDE_PATH);
 
-        $server->push($request->fd, $data);
+        $msg = ['type'=>'quiz-json', 'payload'=>'data'];
+
+        $server->push($request->fd, $msg);
     }
 
 
 
     Public function onMessage(swoole_websocket_server $server, $frame){
         echo "receive from {$frame->fd}: {$frame->data}, opcode:{$frame->opcode}, fin: {$frame->finish}\n";
-        $data = file_get_contents($this->file, FILE_USE_INCLUDE_PATH);
-        $data = json_decode($data,true);
-        if(array_key_exists($frame->data,$data)) $data[$frame->data] = $data[$frame->data] + 1;
-        else $data[$frame->data] = 1;
+        // $data = file_get_contents($this->file, FILE_USE_INCLUDE_PATH);
+        // $data = json_decode($data,true);
+        // if(array_key_exists($frame->data,$data)) $data[$frame->data] = $data[$frame->data] + 1;
+        // else $data[$frame->data] = 1;
 
-        $enc_data = json_encode($data);
+        // $enc_data = json_encode($data);
 
-        foreach($server->connections as $fd){
-            $server->push($fd, $enc_data);
-        }
-        file_put_contents($this->file, $enc_data);
+        // foreach($server->connections as $fd){
+        //     $server->push($fd, $enc_data);
+        // }
+        // file_put_contents($this->file, $enc_data);
 
     }
 
@@ -54,5 +56,7 @@ Class Server
         Echo "Client{$fd}Connection close\n";
     }
 }
+echo "starting server...";
 
 $server = New Server();
+echo "serving...";
